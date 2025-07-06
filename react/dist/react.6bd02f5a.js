0b53ba702e88e140d3be9437d375058e3e54f35d
Jayashree <s.jayashree94@gmail.com>
2025-07-06 06:21:25 +0530

    second commit

---
+++ react/dist/react.6bd02f5a.js
@@ -0,0 +1,5 @@
+const parent = React.createElement("h1", {}, "hello");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render("parent");
+
+//# sourceMappingURL=react.6bd02f5a.js.map
