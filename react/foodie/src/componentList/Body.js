d878ece09e664d6081e4e2adbf0cd9d39cf9c0ae
Jayashree <s.jayashree94@gmail.com>
2025-07-09 06:21:48 +0530

    reconciliation

--- react/foodie/src/componentList/Body.js
+++ react/foodie/src/componentList/Body.js
@@ -1,12 +1,21 @@
 import { resList } from '../utils/MockData';
 import RestaurantCard from "./RestaurantCard";
+import { useState } from 'react';
 
 const Body =() =>{
+
+  const [filteredRestaurant, setfilteredRestaurant] = useState(resList);
+ const handleClick=()=>{
+  const topList = resList.filter((restaurant) => restaurant.info.avgRating > 4.5 )
+  setfilteredRestaurant(topList);
+  };
   return(
     <div className="body">
-      <div className='search'>Search</div>
+      <div className='filter'>
+        <button className='toprated' onClick={handleClick}>Top Rated Restaurant</button>
+      </div>
       <div className='res-container'>
-      {resList.map((restaurant) => (<RestaurantCard key = {restaurant.info.id} resData={restaurant}/>))}
+      {filteredRestaurant.map((restaurant) => (<RestaurantCard key = {restaurant.info.id} resData={restaurant}/>))}
       </div>
     </div>
   )
