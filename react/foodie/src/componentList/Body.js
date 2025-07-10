d5a3afc10d8d91e1d7d7573123ef4305aec7a943
Jayashree <s.jayashree94@gmail.com>
2025-07-10 06:00:40 +0530

    header-change

--- react/foodie/src/componentList/Body.js
+++ react/foodie/src/componentList/Body.js
@@ -1,8 +1,10 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
 const [filteredRestaurant, setfilteredRestaurant] = useState([]);
+
 
 useEffect(()=>{
 fetchData();
@@ -17,14 +19,12 @@ const handleClick = () => {
 
 const fetchData= async()=>{
   const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.08950&lng=80.27390&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"); 
-
   const json = await data.json();
   console.log(json);
-  setfilteredRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+  setfilteredRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
 }
 
-
-  return (
+  return filteredRestaurant.length === 0 ? <Shimmer/> : (
     <div className="body">
       <div className="filter">
         <button className="toprated" onClick={handleClick}>
--- react/foodie/src/componentList/Header.js
+++ react/foodie/src/componentList/Header.js
@@ -1,5 +1,16 @@
+import { useState } from "react";
 const Header =() =>{
+
+  const [btnName, setBtnName] = useState('login');
+
+  const toggleButton=()=>{
+    btnName === 'login' ? 
+    setBtnName("logout") : 
+    setBtnName("login");
+  }
+
   return(
+
     <div className="header">
       <div>
         <img className= "logo" src="https://static.vecteezy.com/system/resources/previews/011/405/724/original/call-food-logo-design-food-delivery-service-logo-concept-free-vector.jpg" alt="logo"></img>
@@ -10,7 +21,8 @@ const Header =() =>{
         <li>Home</li>
         <li>Contact US</li>
         <li>About US</li>
-        <li>CART</li>
+        <li>Cart</li>
+        <button className="login-btn" onClick={toggleButton}> {btnName} </button>
       </ul>
       </div>
      
