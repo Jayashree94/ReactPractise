fb8134893d5ed17a212626bc3991b8cac616f586
Jayashree <s.jayashree94@gmail.com>
2025-07-10 06:33:19 +0530

    search feature included

--- react/foodie/src/componentList/Body.js
+++ react/foodie/src/componentList/Body.js
@@ -3,30 +3,58 @@ import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
-const [filteredRestaurant, setfilteredRestaurant] = useState([]);
+  const [filteredRestaurant, setfilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-useEffect(()=>{
-fetchData();
-},[]);
-
-const handleClick = () => {
+  const handleClick = () => {
     const topList = filteredRestaurant.filter(
       (restaurant) => restaurant.info.avgRating > 4.5
     );
     setfilteredRestaurant(topList);
   };
 
-const fetchData= async()=>{
-  const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.08950&lng=80.27390&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"); 
-  const json = await data.json();
-  console.log(json);
-  setfilteredRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-}
+  const handleSearch = () => {
+    const searchRestaurant = filteredRestaurant.filter((restaurant) => restaurant.info.name.includes(searchText));
+    setfilteredRestaurant(searchRestaurant);
 
-  return filteredRestaurant.length === 0 ? <Shimmer/> : (
+  };
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.08950&lng=80.27390&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setfilteredRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return filteredRestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          ></input>
+          <button
+            className="search-btn"
+            onClick={(searchText) => {
+              handleSearch();
+            }}>
+            Search
+          </button>
+        </div>
+
         <button className="toprated" onClick={handleClick}>
           Top Rated Restaurant
         </button>
