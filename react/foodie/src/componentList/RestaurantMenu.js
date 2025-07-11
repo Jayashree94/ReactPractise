7db3208970390b54cdd9ee4b6d6eca76325f44bb
Jayashree <s.jayashree94@gmail.com>
2025-07-12 04:40:23 +0530

    rest menu

--- react/foodie/src/componentList/RestaurantMenu.js
+++ react/foodie/src/componentList/RestaurantMenu.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
 const RestaurantMenu = () => {
-  const [resinfo, setResInfo] = useState([]);
+  const [resinfo, setResInfo] = useState(null);
+
   useEffect(() => {
     fetchMenu();
   }, []);
@@ -13,25 +13,47 @@ const RestaurantMenu = () => {
       "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0843007&lng=80.2704622&restaurantId=363359&catalog_qa=undefined&submitAction=ENTER"
     );
     const json = await data.json();
-    
     setResInfo(json.data);
-  
   };
-    console.log(resinfo?.cards[2]?.card?.card?.info.name)
-    //const {name} = resinfo?.cards[2]?.card?.card?.info;
-  
-    
-  return resinfo === null ? (
-    <Shimmer />
-  ) : (
-    <div className="menu">
-       <h2>{resinfo?.cards[2].card.card.info.name}</h2>
-       <h3>{resinfo?.cards[2].card.card.info.areaName}</h3>
-       <h4>{resinfo?.cards[2].card.card.info.costForTwoMessage}</h4>
-       <h4>{resinfo?.cards[2].card.card.info.cuisines.join(", ")}</h4>
 
+  if (resinfo === null) return <Shimmer />;
+
+  // Get restaurant info
+  const restaurantCard = resinfo?.cards?.find(
+    (card) =>
+      card?.card?.card?.["@type"] ===
+      "type.googleapis.com/swiggy.presentation.food.v2.Restaurant"
+  );
+
+  const { name, cuisines, locality } = restaurantCard?.card?.card?.info || {};
+
+  // Get menu items from REGULAR section
+  const itemCategory = resinfo?.groupedCard?.cardGroupMap?.REGULAR?.cards?.find(
+    (card) =>
+      card?.card?.card?.["@type"] ===
+      "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+  );
+
+  const itemCards = itemCategory?.card?.card?.itemCards || [];
+
+  return (
+    <div className="menu">
+      <h1>{name}</h1>
+      <h2>{cuisines?.join(", ")}</h2>
+      <h3>{locality}</h3>
+
+      <h2>Menu</h2>
       <ul>
-        <li>{}</li>
+        {itemCards.map((item) => {
+          const info = item.card.info;
+          return (
+            <li key={info.id}>
+              <h4>{info.name}</h4>
+              <p>{info.description}</p>
+              <p>₹{info.price / 100}</p>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
